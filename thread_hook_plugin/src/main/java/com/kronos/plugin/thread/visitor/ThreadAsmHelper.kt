4a215f406dc9c94d547c9bb36e3a33437db1de7b
liabao <zhangyang06@bilibili.com>
2020-10-15 11:52:28 +0800

    [feat] thread executor hoook

--- thread_hook_plugin/src/main/java/com/kronos/plugin/thread/visitor/ThreadAsmHelper.kt
+++ thread_hook_plugin/src/main/java/com/kronos/plugin/thread/visitor/ThreadAsmHelper.kt
@@ -2,6 +2,7 @@ package com.kronos.plugin.thread.visitor
 
 import com.kronos.plugin.base.AsmHelper
 import com.kronos.plugin.base.Log.info
+import com.kronos.plugin.thread.PoolEntity.Companion.Owner
 import com.kronos.plugin.thread.visitor.ThreadPoolCreator.EXECUTORS_OWNER
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
@@ -41,13 +42,19 @@ class ThreadAsmHelper : AsmHelper {
         return classWriter.toByteArray()
     }
 
-    fun MethodInsnNode.hookExecutors(classNode: ClassNode, methodNode: MethodNode) {
-        info("owner:$owner  name:$name")
+    private fun MethodInsnNode.hookExecutors(classNode: ClassNode, methodNode: MethodNode) {
         when (this.owner) {
             EXECUTORS_OWNER -> {
-                when (this.name) {
-
+                info("owner:${this.owner}  name:${this.name} ")
+                ThreadPoolCreator.poolList.forEach {
+                    if (it.name == this.name && this.name == it.name && this.owner == it.owner) {
+                        this.owner = Owner
+                        this.name = it.methodName
+                        this.desc = it.replaceDesc()
+                        info("owner:${this.owner}  name:${this.name} desc:${this.desc} ")
+                    }
                 }
+
             }
         }
     }
