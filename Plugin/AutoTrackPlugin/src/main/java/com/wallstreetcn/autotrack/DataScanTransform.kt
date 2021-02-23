dc63006427dddba8fcfb4713d48c73bfcc653957
Leifzhang <454327998@qq.com>
2021-02-23 23:44:35 +0800

    [feat] 优化编译速度  将jar 扫描更换到多线程内操作

---
+++ Plugin/AutoTrackPlugin/src/main/java/com/wallstreetcn/autotrack/DataScanTransform.kt
@@ -0,0 +1,52 @@
+package com.wallstreetcn.autotrack
+
+import com.android.build.api.transform.QualifiedContent
+import com.android.build.api.transform.Transform
+import com.android.build.api.transform.TransformException
+import com.android.build.api.transform.TransformInvocation
+import com.android.build.gradle.internal.pipeline.TransformManager
+import com.kronos.plugin.base.BaseTransform
+import com.kronos.plugin.base.ClassUtils
+import com.kronos.plugin.base.TransformCallBack
+import com.wallstreetcn.autotrack.scan.DataScanAsmHelper
+import java.io.IOException
+
+/**
+ * @Author LiABao
+ * @Since 2021/1/9
+ */
+class DataScanTransform : Transform() {
+    override fun getName(): String {
+        return "DataScanTransform"
+    }
+
+    override fun getInputTypes(): Set<QualifiedContent.ContentType> {
+        return TransformManager.CONTENT_JARS
+    }
+
+    override fun getScopes(): MutableSet<in QualifiedContent.Scope>? {
+        return TransformManager.SCOPE_FULL_PROJECT
+    }
+
+    override fun isIncremental(): Boolean {
+        return true
+    }
+
+    @Throws(TransformException::class, InterruptedException::class, IOException::class)
+    override fun transform(transformInvocation: TransformInvocation) {
+        val injectHelper = DataScanAsmHelper()
+        val baseTransform = BaseTransform(transformInvocation, object : TransformCallBack {
+            override fun process(className: String, bytes: ByteArray?): ByteArray? {
+                if (ClassUtils.checkClassName(className)) {
+                    try {
+                        return injectHelper.modifyClass(bytes)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+                return null
+            }
+        })
+        baseTransform.startTransform()
+    }
+}
---
+++ Plugin/double_tap_plugin/src/main/java/com/kronos/doubletap/DoubleTapTransform.kt
@@ -0,0 +1,42 @@
+package com.kronos.doubletap
+
+import com.android.build.api.transform.Transform
+import com.android.build.api.transform.TransformException
+import com.android.build.api.transform.TransformInvocation
+import com.kronos.doubletap.helper.DoubleTapAsmHelper
+import com.kronos.plugin.base.BaseTransform
+import com.kronos.plugin.base.ClassUtils
+import com.kronos.plugin.base.TransformCallBack
+import java.io.IOException
+
+abstract class DoubleTapTransform : Transform() {
+    override fun getName(): String {
+        return "DoubleTapTransform"
+    }
+
+    override fun isIncremental(): Boolean {
+        return true
+    }
+
+    @Throws(TransformException::class, InterruptedException::class, IOException::class)
+    override fun transform(transformInvocation: TransformInvocation) {
+        val injectHelper = DoubleTapAsmHelper()
+        val baseTransform = BaseTransform(transformInvocation, object : TransformCallBack {
+            override fun process(className: String, classBytes: ByteArray?): ByteArray? {
+                if (ClassUtils.checkClassName(className)) {
+                    try {
+                        return injectHelper.modifyClass(classBytes)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+                return null
+            }
+        })
+        baseTransform.startTransform()
+    }
+
+    override fun isCacheable(): Boolean {
+        return true
+    }
+}
